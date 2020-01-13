75c3635a5e8e1b2e3f056c439d618f783aac7bd4
Yevhen Konyk <yevhen.konyk@gmail.com>
2020-01-13 17:51:59 +0200

    Updates

---
+++ precache-manifest.245c4d71177191fbb8ef067798dddbf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05c9a4d7fd3dddc7fadd2ad022b88b3",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "2c020db4c6675175df45",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.e5baa8f2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.e5baa8f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "24851180098ba852dcc2",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.539ffe87.chunk.js"
+  },
+  {
+    "revision": "379ef3b0febdda6c830e",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.752c2966.js"
+  }
+]);
